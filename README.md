4c4d3e3c3f6216e103024420dd639e7eb67e05ab
Shahidul Alam Riyad <riyadnoapara@gmail.com>
2023-08-29 00:40:24 +0600

    feat: props, readme, package.json updated

--- README.md
+++ README.md
@@ -14,8 +14,6 @@
 
 `react-audio-play` is a simple, lightweight and customizable audio player npm package for React applications. It provides an easy-to-use interface to play audio files in your React components with minimal setup.
 
-<!-- ![react-audio-play](https://example.com/react-audio-play-demo.gif) -->
-
 ## Features
 
 - Play audio files in your React application with a single component.
@@ -27,80 +25,6 @@
 ## Demo
 Check [examples](https://riyaddecoder.github.io/react-audio-play/#example-usage) 
 
-## Installation
-
-You can install `react-audio-play` using npm or yarn:
-
-```bash
-npm install react-audio-play
-```
-or
-```bash
-yarn add react-audio-play
-```
-
-## Usage
-
-To use `react-audio-play`, import the `AudioPlayer` component and provide the necessary props:
-
-```js
-import React from 'react';
-import { AudioPlayer } from 'react-audio-play';
-
-const App = () => {
-  return (
-    <div>
-      <h1>My Audio Player</h1>
-      <AudioPlayer src="path/to/audio.mp3" />
-    </div>
-  );
-};
-
-export default App;
-```
-## Props
-`react-audio-play` accepts the following props:
-
-- `src` (string, required): The URL or file path of the audio file to be played.
-- `loop` (boolean, optional): Set to true to loop the audio playback (default: false).
-- `volume` (number, optional): The initial volume level (0 to 100) of the audio (default: `100`).
-- `onPlay` (function, optional): Callback function to execute when the audio starts playing.
-- `onPause` (function, optional): Callback function to execute when the audio is paused.
-- `onEnd` (function, optional): Callback function to execute when the audio playback ends.
-- `onError` (function, optional): Callback function to execute if there's an error loading or playing the audio.
-- `backgroundColor` (string, optional): Set the background color of the audio player (default: `#fff`).
-- `color` (string, optional): The text and icon color of the audio player. (default: `#566574`).
-- `sliderColor` (string, optional): The color of the progress slider. (default: `#007FFF`)
-- `volumePlacement` (string, optional): Control where the volume controls are located (`top | bottom`).(default: `top`)
-
-## Example with Custom Event Handling
-```js
-import React from 'react';
-import { AudioPlayer } from 'react-audio-play';
-
-const App = () => {
-  const handlePlay = () => {
-    console.log('Audio started playing');
-  };
-
-  const handlePause = () => {
-    console.log('Audio paused');
-  };
-
-  return (
-    <div>
-      <h1>My Audio Player</h1>
-      <AudioPlayer
-        src="path/to/audio.mp3"
-        onPlay={handlePlay}
-        onPause={handlePause}
-      />
-    </div>
-  );
-};
-
-export default App;
-```
 ## Custom Styling
 You can easily customize the appearance of the audio player by applying your CSS styles to the AudioPlayer component or by overriding the default styles in your project's CSS. Check [examples](https://riyaddecoder.github.io/react-audio-play/#styled-examples)
 
@@ -109,7 +33,5 @@ This package is open-source and distributed under the MIT License. See the [LICE
 
 ## Contribution
 Contributions are welcome! If you find any issues or have suggestions, feel free to create an issue or submit a pull request on the [GitHub repository](https://github.com/riyaddecoder/react-audio-play/). Let's build this package together!
-
-<!-- ![react-audio-play](https://example.com/react-audio-play-demo.gif) -->
 
 Enjoy using react-audio-play in your React applications, and we hope it enhances your audio experience with ease and flexibility. If you have any questions or need further assistance, don't hesitate to reach out to us! Happy coding! 🎶
--- src/components/PropsBlock.tsx
+++ src/components/PropsBlock.tsx
@@ -8,38 +8,55 @@ const PropsBlock = () => {
         The <code className="bg-gray-100 p-1">AudioPlayer</code> component accepts the following props:
       </p>
       <ul className="list-disc list-inside mt-2">
-        <li>
+        <li className="mt-2">
+          <code className="bg-gray-100 p-1">className</code> (string, optional): A CSS class name for styling the component.
+        </li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">src</code> (string, required): The URL or file path of the audio file to be played.
         </li>
-        <li>
+        <li className="mt-2">
+          <code className="bg-gray-100 p-1">preload</code> (string, optional): Specifies the preload behavior for the audio file. Possible values are.
+          <ul className="list-disc list-inside mt-2">
+            <li className="ml-4 mt-1">
+              <code className="bg-gray-100 p-1">auto</code> The audio data is loaded as soon as possible.
+            </li>
+            <li className="ml-4 mt-1">
+              <code className="bg-gray-100 p-1">metadata</code> Only metadata (e.g., duration) is loaded.
+            </li>
+            <li className="ml-4 mt-1">
+              <code className="bg-gray-100 p-1">none</code> No preloading. Audio data is only loaded when requested.
+            </li>
+          </ul>
+        </li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">loop</code> (boolean, optional): Set to <code className="bg-gray-100 p-1">true</code> to loop the audio playback (default:{' '}
           <code className="bg-gray-100 p-1">false</code>).
         </li>
-        <li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">volume</code> (number, optional): The initial volume level (0 to 100) of the audio (default: <code className="bg-gray-100 p-1">100</code>).
         </li>
-        <li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">onPlay</code> (function, optional): Callback function to execute when the audio starts playing.
         </li>
-        <li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">onPause</code> (function, optional): Callback function to execute when the audio is paused.
         </li>
-        <li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">onEnd</code> (function, optional): Callback function to execute when the audio playback ends.
         </li>
-        <li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">onError</code> (function, optional): Callback function to execute if there&quot;s an error loading or playing the audio.
         </li>
-        <li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">backgroundColor</code> (string, optional): Set the background color of the audio player (default: <code className="bg-gray-100 p-1">#fff</code>).
         </li>
-        <li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">color</code> (string, optional): The text and icon color of the audio player (default: <code className="bg-gray-100 p-1">#566574</code>).
         </li>
-        <li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">sliderColor</code> (string, optional): The color of the progress slider (default: <code className="bg-gray-100 p-1">#007FFF</code>).
         </li>
-        <li>
+        <li className="mt-2">
           <code className="bg-gray-100 p-1">volumePlacement</code> (string, optional): Control where the volume controls are located (<code className="bg-gray-100 p-1">top | bottom</code>) (default:{' '}
           <code className="bg-gray-100 p-1">bottom</code>).
         </li>
